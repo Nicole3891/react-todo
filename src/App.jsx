02c1fc73a4722a0c9feab34f3caa74639c157741
Nastase Nicoleta <nastasenicoleta@Nastases-MacBook-Pro.local>
2024-02-23 09:08:24 -0500

    changes to jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+const todoList = [
+  {
+    objectID: 1,
+    title: "Create a repo on GitHub"
+  },
+  {
+    objectID: 2,
+    title: "Create the todo application"
+  },
+  {
+    objectID: 3,
+    title: "Complete assigment"
+  }
+]
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
+    
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+        <h1>Todo list</h1>
+        <label htmlFor="search">Search</label>
+        <input id="search" type="text"/>
+      
+      <hr/>
+
+      <ul>
+        {todoList.map(function(item) {
+          return (
+            <li key={item.objectID}>{item.title}</li>
+          )
+        })}
+      </ul>
+      </div> 
   )
 }
 
