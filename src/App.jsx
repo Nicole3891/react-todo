e261df71e571b3db708facb17be3a586cc18692f
Nastase Nicoleta <nastasenicoleta@Nastases-MacBook-Pro.local>
2024-03-25 09:58:20 -0400

    rewrite the App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,34 @@
+import React, { useState, useEffect } from 'react';
+import TodoList from './TodoList';
+import AddTodoForm from './AddTodoForm';
 
-    import React, { useState, useEffect } from 'react'
-    import TodoList from './TodoList'
-    import AddToDoForm from './AddTodoForm'
+// Custom Hook for semi-persistent state
+function useSemiPersistentState() {
+  const [todoList, setTodoList] = useState(
+    JSON.parse(localStorage.getItem('savedTodoList')) || []
+  );
 
-  //Custom Hook for semi-persistent state
-  function useSemiPersistentState() {
-    const [ todoList, setTodoList ] = useState (
-      JSON.parse(localStorage.getItem('savedTodoList')) || []
-      ); 
-      useEffect (() => {
-        localStorage.setItem('savedTodoList', JSON.stringify(todoList));
-      }, [todoList]);
+  useEffect(() => {
+    localStorage.setItem('savedTodoList', JSON.stringify(todoList));
+  }, [todoList]);
 
-      return [todoList, setTodoList];
-  }
+  return [todoList, setTodoList];
+}
 
-    function App() {
+function App() {
+  const [todoList, setTodoList] = useSemiPersistentState();
 
-      //1.const [newTodo, setNewTodo] = useState(" "); //create a new state variable
-      //2.Retrive todoList from localStorage or use an empty array if it doesn't exist
-      const [ todoList, setTodoList ] = useSemiPersistentState(); //CALL THE CUSTOM HOOK
-      
-      const addTodo = (newTodo) => {
-        setTodoList([...todoList, newTodo]);
-      };
-      
-      return (
-        
-          <>
-            <h1>Todo List</h1>
+  const addTodo = (newTodo) => {
+    setTodoList([...todoList, newTodo]);
+  };
 
-    {/* Using AddToDoForm component*/}  
-    <AddToDoForm  onAddTodo={addTodo}/>
-    {/* Using TodoList component*/}
-      <TodoList list={todoList}/> 
+  return (
+    <>
+      <h1>Todo List</h1>
+      <AddTodoForm onAddTodo={addTodo} />
+      <TodoList todoList={todoList} />
+    </>
+  );
+}
 
-
-        
-          </> 
-      );
-    }
-
-    export default App;
+export default App;
